3a3a1201a0e64af99cfb4cbe2d30ecda82f2c503
Sakshi Shah <sakshishahjs12@gmail.com>
2026-04-12 15:37:35 -0400

    changes in backend

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from backend.agents.agent import run_agent
+from agents.agent import run_agent
 
 app = FastAPI()
 
